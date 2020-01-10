5a9b137eed101fbaa3e6c7706a7d080e8cea6856
ibraphem <ibraphem@gmail.com>
2020-01-10 10:11:18 +0100

    Reciept with navbar print button

--- resources/views/sale/complete.blade.php
+++ resources/views/sale/complete.blade.php
@@ -1,209 +1,15 @@
-<!DOCTYPE HTML>
-<html>
-<head>
-	<meta http-equiv="content-type" content="text/html" />
-	<meta name="author" content="lolkittens" />
-     <meta name="viewport" content="width=device-width,initial-scale=1.0">
-<!--    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>-->
+@extends('layouts.admin')
 
-
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-
-<!-- jQuery library -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-
-<!-- Popper JS -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-
-<!-- Latest compiled JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-
-<!------ Include the above in your HEAD tag ---------->
-
-<!--Author      : @arboshiki-->
-    <link rel="stylesheet" type="text/css"  href="css/bootstrap.css"/>
-    <link rel="stylesheet" type="text/css"  href="css/bootstrap.min.css"/>
-    <link rel="stylesheet" type="text/css"  href="css/bootstrap-grid.css"/>
-    <link rel="stylesheet" type="text/css"  href="css/bootstrap-reboot.css"/>
-    
-
-	<title>YAREECEE SALES/STOCK INVENTORY MANAGER</title>
-    <style>
-    
-    
-    #inventory-invoice{
-    padding: 30px;
-}
-#inventory-invoice a{text-decoration:none !important;}
-.invoice {
-    position: relative;
-    background-color: #FFF;
-    min-height: 680px;
-    padding: 15px
-}
-
-.invoice header {
-    padding: 10px 0;
-    margin-bottom: 20px;
-    border-bottom: 1px solid #3989c6
-}
-
-.invoice .company-details {
-    text-align: right
-}
-
-.invoice .company-details .name {
-    margin-top: 0;
-    margin-bottom: 0
-}
-
-.invoice .contacts {
-    margin-bottom: 20px
-}
-
-.invoice .invoice-to {
-    text-align: left
-}
-
-.invoice .invoice-to .to {
-    margin-top: 0;
-    margin-bottom: 0
-}
-
-.invoice .invoice-details {
-    text-align: right
-}
-
-.invoice .invoice-details .invoice-id {
-    margin-top: 0;
-    color: #3989c6
-}
-
-.invoice main {
-    padding-bottom: 50px
-}
-
-.invoice main .thanks {
-    margin-top: -100px;
-    font-size: 2em;
-    margin-bottom: 50px
-}
-
-.invoice main .notices {
-    padding-left: 6px;
-    border-left: 6px solid #3989c6
-}
-
-.invoice main .notices .notice {
-    font-size: 1.2em
-}
-
-.invoice table {
-    width: 100%;
-    border-collapse: collapse;
-    border-spacing: 0;
-    margin-bottom: 20px
-}
-
-.invoice table td,.invoice table th {
-    padding: 15px;
-    background: #eee;
-    border-bottom: 1px solid #fff
-}
-
-.invoice table th{
-    white-space: nowrap;
-    font-weight: 400;
-    font-size: 16px;
-    border:1px solid #fff;
-}
-.invoice table td{
-    border:1px solid #fff;
-}
-.invoice table td h3 {
-    margin: 0;
-    font-weight: 400;
-    color: #3989c6;
-    font-size: 1.2em
-}
-
-.invoice table .tax,.invoice table .total,.invoice table .unit {
-    text-align: right;
-    font-size: 1.2em
-}
-
-.invoice table .no {
-    color: #fff;
-    font-size: 1.6em;
-    background: #0c0028
-}
-
-.invoice table .unit {
-    background: #ddd
-}
-
-.invoice table .total {
-    background: #0c0028;
-    color: #fff
-}
-
-.invoice table tfoot td {
-    background: 0 0;
-    border-bottom: none;
-    white-space: nowrap;
-    text-align: right;
-    padding: 10px 20px;
-    font-size: 1.2em;
-    border-top: 1px solid #aaa
-}
-
-.invoice table tfoot tr:first-child td {
-    border-top: none
-}
-
-.invoice table tfoot tr:last-child td {
-    color: #3989c6;
-    font-size: 1.4em;
-    border-top: 1px solid #3989c6
-}
-
-.invoice table tfoot tr td:first-child {
-    border: none
-}
-
-.invoice footer {
-    width: 100%;
-    text-align: center;
-    color: #777;
-    border-top: 1px solid #aaa;
-    padding: 8px 0
-}
-
-/*@media print {
-    .invoice {
-        font-size: 11px!important;
-        overflow: hidden!important;
-    }*/
-    
-
-
-    .invoice footer {
-        position: absolute;
-        margin-top: 400px;
-        page-break-after: always;
-    }
-
-    .invoice>div:last-child {
-        page-break-before: always
-    }
-}
-    </style>
-</head>
-
-<body>
-    <div class="container-fluid">
+@section('content')
+<div class="content-wrapper sale-background" ng-app="tutapos">
+    <!-- Content Header (Page header) -->
+    <section class="content-header" >
+      <h1>{{__('Sales/Invoice')}}</h1>
+      
+    </section>
+    <!-- Main content -->
+    <section class="content panel" >
+            <div class="container-fluid">
 <div id="inventory-invoice">
 
 
@@ -212,10 +18,10 @@
             <header>
                 <div class="row">
                     <div class="col">
-                    <img src="{{asset('images/yareecee1.png')}}" alt="receipt-logo" style="height: 150px;  margin-top: -25px;">
+                    <img src="{{asset('images/yareecee1.png')}}" alt="receipt-logo" style="height: 70px;  margin-top: -40px;">
                          <!--   <img src="asset/images/yareecee1.png" style="height: 150px;" /> -->
                     </div>
-                    <div class="col company-details" style="margin-top: -45px;">
+                    <div class="col company-details" style="margin-top: -100px;">
                     <sup style="color: red;"><b>REG: 887372</b></sup>
                         <h2 class="name" style="color: #0c0028;" >
                             
@@ -239,7 +45,7 @@
                         <div class="email">{{trans('sale.payment_type')}}: {{$sales->payment_type}}</div>
                         
                     </div>
-                    <div class="col invoice-details">
+                    <div class="col invoice-details" style="margin-top: -100px">
                         <h2 class="invoice-id" style="color: #0c0028;">YAR00{{$saleItemsData->sale_id}}</h2>
                         <div class="date">Date: {{ Carbon\Carbon::now() }}</div>
                         <div class="date">Attendant: {{$sales->user->name}}</div>
@@ -295,18 +101,7 @@
                     </tfoot>
                 </table>
             </div>
-                <div style="color: #0c0028;"><h3>Thank you!</h3></div>
-                <div class="notices">
-                    <div style="color: #0c0028;"><b>No return of money after payment.</b></div>
-                    <div class="notice">Goods sold in good condition are not returnable.</div>
-                </div>
-            </main>
-            <footer>
-                 <dl class="dl-horizontal" style="margin-bottom: 40px;">
-                <div style="color: red;" ><b>Dealers In All Kinds Of Electronic Appliances And General Contractors</b></div>
-                <div style="color: red;"><b>Refridgerator, Freezers, Home Theatre, Generator, LCD TV, Air Condition,Electric Iron, Blender Kettle ETC</b></div>
-            </dl>
-            </footer>
+        
         </div>
         <div></div>
     </div>
@@ -322,24 +117,21 @@
             }
         });
 </script>-->
-<hr>
 <div class="toolbar hidden-print">
-        <div class="text-right" style="margin-right: 40px;">
-            <button id="printInvoice" class="btn btn-info"><i class="fa fa-print"></i> Print</button>
+        <div class="text-right" style="margin-right: 40px; margin-top: -30px">
+            <button id="printInvoice" onclick="printInvoice()" class="btn btn-info"><i class="fa fa-print"></i> Print</button>
             <a href="{{ url('/sales') }}" type="button" class="btn btn-primary">{{trans('sale.new_sale')}}</a>
            <!-- <button class="btn btn-info"><i class="fa fa-file-pdf-o"></i> Export as PDF</button> -->
         </div>
     </div>
-    <script>
- $('#printInvoice').click(function(){
-            Popup($('.invoice')[0].outerHTML);
-            function Popup(data) 
-            {
-                window.print();
-                return true;
-            }
-        });
-</script>
+
 </div>
-</body>
-</html>
+    </section>
+</div>
+<script>
+function printInvoice() {
+  window.print();
+}
+</script>
+@endsection
+
